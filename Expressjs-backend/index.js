2606a3363856f55bc907599f9abfda6e8052544f
Pratik Sharma <149568080+Coder-Pratik-Kumar@users.noreply.github.com>
2026-02-03 11:48:26 +0530

    [Pratik] Add. learning about post method and thunder client

--- Expressjs-backend/index.js
+++ Expressjs-backend/index.js
@@ -33,11 +33,14 @@ app.get("/users",(req,res)=>{
     res.send("<h1>this is users page</h1> ")
 })
 const students=[
-    {id:1,name:"Alice",branch:"CS"},
-    {id:2,name:"Bob",branch:"EC"},
-    {id:3,name:"Crisi",branch:"EE"},
-    {id:4,name:"steve",branch:"ME"}
-
+    { id: 1, name: "Alice", branch: "CS" },
+    
+    { id: 2, name: "Bob", branch: "EC" },
+    
+    { id: 3, name: "Crisi", branch: "EE" },
+    
+    { id: 4, name: "steve", branch: "ME" }
+    
 ]
 app.get("/users/:id",(req,res)=>{
     const userId=req.params.id
@@ -45,6 +48,9 @@ app.get("/users/:id",(req,res)=>{
 })
 app.get("/students/search", (req, res) => {
     const branch = req.query.branch;
+    if (!branch) {
+        return res.json(students);
+    }
     const foundStudent = students.filter(s => s.branch == branch);
     res.json(foundStudent);
 })
@@ -54,9 +60,13 @@ app.get("/students",(req,res)=>{
 app.get("/students/:id", (req, res) => {
     const id = req.params.id;
     const arrayIndex = students.findIndex(s => s.id == id);
-
     const data = students[arrayIndex];
     res.json(data);
+})
+app.post("/students/register", (req, res) => {
+    const data = req.body;
+    students.push(data);
+    res.json(students);
 })
 app.listen(PORT,()=>{
     console.log(`Server is Running on port:${PORT}`)
